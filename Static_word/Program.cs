7732394b7ce73ba95e2e25c477674aed240bd78e
Alona <122108963+yelloally@users.noreply.github.com>
2023-02-13 12:45:43 +0100

    Add files via upload

---
+++ Static_word/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticModifierExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The result of the static method is: " + Utility.Square(5));
+            Console.ReadLine();
+        }
+    }
+
+    static class Utility
+    {
+        public static int Square(int number)
+        {
+            return number * number;
+        }
+    }
+}
